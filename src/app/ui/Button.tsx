6c2701c05c82dafc677a25ebfd11e7491ca33fe0
chandraanggaradiputra <anggarasixteen@gmail.com>
2026-08-11 15:19:28 +0700

    Revisi Tombol

--- src/app/ui/Button.tsx
+++ src/app/ui/Button.tsx
@@ -14,7 +14,7 @@ const Button: React.FC<ButtonProps> = ({ href, target, rel, CTA, logo }) => {
     <button className="bg-blue-500 hover:bg-blue-700 px-4 py-2 rounded font-bold text-white transition-all duration-300 ease-in-out">
       <Link href={href} target={target} rel={rel}>
         <Image
-          src={logo}
+          src={logo || "/svg/logo-whatsapp.svg"}
           alt="Logo"
           width={20}
           height={20}
